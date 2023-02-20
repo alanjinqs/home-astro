18b86c82345cc94a3bf5fd89dc5e722bdafca141
Alan Jin <jin.qiushi@outlook.com>
2023-02-20 16:27:18 +0000

    add vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import {
-  defineConfig
-} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
-    },
-  }), vue()]
+    }
+  }), vue()],
+  output: "server",
+  adapter: vercel()
 });
