e142d02769946afbca2a22e3ff7952f49454645a
Alan Jin <o@alanj.in>
2024-05-06 12:46:21 +0100

    feat: 🎸 add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import {
-  defineConfig
-} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import vercel from "@astrojs/vercel/serverless";
-
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
@@ -14,12 +10,15 @@ import rehypeKatex from "rehype-katex";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), vue(), mdx()],
+  }), vue(), mdx(), react()],
   output: "server",
   markdown: {
     remarkPlugins: [remarkMath],
